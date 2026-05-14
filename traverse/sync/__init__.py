8343922562fe6b5b633e428c96a4dd4b377240ff
Japneet Kalkat <japneet.kalkat@trailflow.ai>
2026-05-14 19:50:36 -0300

    Add agentyc/browser/watchdogs/recording_watchdog.py

--- traverse/sync/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Cloud sync module for Traverse."""
-
-from traverse.sync.auth import CloudAuthConfig, DeviceAuthClient
-from traverse.sync.service import CloudSync
-
-__all__ = ['CloudAuthConfig', 'DeviceAuthClient', 'CloudSync']
