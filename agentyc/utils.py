ee6137f2626f210785e9591b1a8cb122e5e004ba
Japneet Kalkat <japneet.kalkat@trailflow.ai>
2026-05-15 08:27:45 -0300

    feat: update utils.py

--- agentyc/utils.py
+++ agentyc/utils.py
@@ -1,25 +1,26 @@
-from agentyc._utils_async import create_task_with_error_handling
-from agentyc._utils_async import singleton
-from agentyc._utils_async import time_execution_async
-from agentyc._utils_async import time_execution_sync
-from agentyc._utils_core import URL_PATTERN
-from agentyc._utils_core import _get_groq_bad_request_error
-from agentyc._utils_core import _get_openai_bad_request_error
-from agentyc._utils_core import logger
-from agentyc._utils_runtime import check_env_variables
-from agentyc._utils_runtime import check_latest_agentyc_version
-from agentyc._utils_runtime import get_agentyc_version
-from agentyc._utils_runtime import get_git_info
-from agentyc._utils_runtime import merge_dicts
+from agentyc._utils_async import (
+	create_task_with_error_handling,
+	singleton,
+	time_execution_async,
+	time_execution_sync,
+)
+from agentyc._utils_core import URL_PATTERN, _get_groq_bad_request_error, _get_openai_bad_request_error, logger
+from agentyc._utils_runtime import (
+	check_env_variables,
+	check_latest_agentyc_version,
+	get_agentyc_version,
+	get_git_info,
+	merge_dicts,
+)
 from agentyc._utils_signals import SignalHandler
-from agentyc._utils_strings import collect_sensitive_data_values
-from agentyc._utils_strings import redact_sensitive_string
-from agentyc._utils_strings import sanitize_surrogates
-from agentyc._utils_urls import _log_pretty_path
-from agentyc._utils_urls import _log_pretty_url
-from agentyc._utils_urls import is_new_tab_page
-from agentyc._utils_urls import is_unsafe_pattern
-from agentyc._utils_urls import match_url_with_domain_pattern
+from agentyc._utils_strings import collect_sensitive_data_values, redact_sensitive_string, sanitize_surrogates
+from agentyc._utils_urls import (
+	_log_pretty_path,
+	_log_pretty_url,
+	is_new_tab_page,
+	is_unsafe_pattern,
+	match_url_with_domain_pattern,
+)
 
 __all__ = [
 	'URL_PATTERN',
